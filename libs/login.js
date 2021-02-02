f99c49176ead06b98a0b960ee1c6dbc7b4941bf9
selenamona <1591541371@qq.com>
2021-02-02 14:30:13 +0800

    11

---
+++ libs/login.js
@@ -0,0 +1,47 @@
+/**
+ *  小程序登录
+ */
+
+// 登录
+const wxLogin = (callback) => {
+	wx.login({
+		success: (res) => {
+			if (res.code) {
+				callback && callback(res.code);
+			} else {
+				wx.showModal({
+					title: '提示',
+					content: '获取用户登录code失败！',
+					showCancel: false
+				})
+			}
+		},
+		fail: () => {
+			wx.showModal({
+				title: '提示',
+				content: '获取用户登录code失败！',
+				showCancel: false
+			})
+		}
+	})
+}
+
+// 检测登录状态
+const checkLoginState = () => {
+	wx.checkSession({
+		success: () => {
+			console.log('登录未过期')
+			return 1;
+		},
+		// 登录过期
+		fail: () => {
+			console.log('登录过期')
+			return 0;
+		}
+	})
+}
+
+module.exports = {
+	wxLogin,
+	checkState
+}
---
+++ unpackage/dist/build/mp-weixin/pages/list/news-item.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["pages/list/news-item"],{4884:function(t,n,e){"use strict";e.r(n);var u=e("edd6"),c=e.n(u);for(var r in u)"default"!==r&&function(t){e.d(n,t,(function(){return u[t]}))}(r);n["default"]=c.a},"6d9a":function(t,n,e){"use strict";e.r(n);var u=e("be21"),c=e("4884");for(var r in c)"default"!==r&&function(t){e.d(n,t,(function(){return c[t]}))}(r);e("c205");var a,f=e("f0c5"),o=Object(f["a"])(c["default"],u["b"],u["c"],!1,null,"4059d23a",null,!1,u["a"],a);n["default"]=o.exports},be21:function(t,n,e){"use strict";var u,c=function(){var t=this,n=t.$createElement;t._self._c},r=[];e.d(n,"b",(function(){return c})),e.d(n,"c",(function(){return r})),e.d(n,"a",(function(){return u}))},c205:function(t,n,e){"use strict";var u=e("cfb2"),c=e.n(u);c.a},cfb2:function(t,n,e){},edd6:function(t,n,e){"use strict";Object.defineProperty(n,"__esModule",{value:!0}),n.default=void 0;var u={name:"newsItem",props:{item:{type:Object,default:function(t){return{}}}},data:function(){return{}},created:function(){},mounted:function(){},methods:{},components:{}};n.default=u}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'pages/list/news-item-create-component',
+    {
+        'pages/list/news-item-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('543d')['createComponent'](__webpack_require__("6d9a"))
+        })
+    },
+    [['pages/list/news-item-create-component']]
+]);
